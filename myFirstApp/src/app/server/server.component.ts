7f35f40ea2aca4564095bb3b57b85c103051b2ce
VladanStar <vladancupric@gmail.com>
2023-02-17 08:16:57 +0100

    Add

---
+++ myFirstApp/src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'appp-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
+})
+export class ServerComponent {}
