e200624b871e45f9b20057d86c1bd991b5ea762c
VladanStar <vladancupric@gmail.com>
2023-02-18 21:18:38 +0100

    Add

--- Primer1/src/app/app.module.ts
+++ Primer1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
