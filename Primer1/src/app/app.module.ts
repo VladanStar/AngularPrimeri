7d4420322b62bf66ef5981988c6bb18d4e27a638
VladanStar <vladancupric@gmail.com>
2023-02-18 22:16:00 +0100

    Add

--- Primer1/src/app/app.module.ts
+++ Primer1/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
@@ -18,7 +18,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule,
+ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Primer1/src/app/product-list/product-list.component.ts
+++ Primer1/src/app/product-list/product-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { map, startWith } from "rxjs/operators";
+import { combineLatest, Observable, of } from "rxjs";
+import { FormControl } from "@angular/forms";
+import { debounceTime, distinctUntilChanged }  from "rxjs/operators";
+import { Country, countries } from "./countries";
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+
+
+  countries$: Observable<Country[]>;
+
+  // Data describing
+  filteredCountry$: Observable<Country[]>;
+
+  filter: FormControl;
+  filter$: Observable<string>;
+
+  constructor() {
+    this.countries$ = of(countries);
+
+    this.filter = new FormControl("");
+    this.filter$ = this.filter.valueChanges.pipe(startWith(""));
+
+    this.filteredCountry$ = combineLatest(this.countries$, this.filter$).pipe(
+      debounceTime(500),
+      map(([countries, filterString]) =>
+        countries.filter(country =>
+          country.name.toLowerCase().includes(filterString)
+        )
+      )
+    );
+  }
 
 }
