70dad1aa3c170d5005d8da15f11a8618a214020d
VladanStar <vladancupric@gmail.com>
2023-02-21 13:38:20 +0100

    Add

--- MoviesAppAngular/src/app/components/tool-bar/tool-bar.component.ts
+++ MoviesAppAngular/src/app/components/tool-bar/tool-bar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tool-bar',
   templateUrl: './tool-bar.component.html',
-  styleUrls: ['./tool-bar.component.css']
+  styleUrls: ['./tool-bar.component.css'],
 })
 export class ToolBarComponent {
+  constructor(private router: Router) {}
+  routes = this.router.config.map((route: Route) => route?.path || 'Home');
+  pageTitle = 'Home';
 
+  ngOnInit(): void {
+    console.log(this.routes);
+  }
+
+  changeRoute(route: string): void {
+    this.pageTitle = route;
+    const pathToGo = route === 'Home' ? '/' : route;
+    this.router.navigate([pathToGo]);
+  }
 }
