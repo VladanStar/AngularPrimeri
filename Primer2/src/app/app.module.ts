12e52dcedc11ac0c2f3abf18afebf9cd03afe72c
VladanStar <vladancupric@gmail.com>
2023-02-21 10:15:16 +0100

    Add

---
+++ Primer2/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Primer2/src/app/student/student.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  templateUrl: './student.component.html',
+  styleUrls: ['./student.component.css'],
+})
+export class StudentComponent {
+  public static readonly listaImena: Array<string> = [
+    'Aleksandar',
+    'Dragan',
+    'Bogdan',
+    'Slobodan',
+    'Milan',
+  ];
+  public static readonly brojImena: number =
+    StudentComponent.listaImena.length;
+
+  public ocene: Array<number> = [];
+  public ime: string = '';
+  public ocenaStr: string = '';
+
+  constructor() {
+    this.ime =
+      StudentComponent.listaImena[
+        Math.floor(Math.random() * StudentComponent.brojImena)
+      ];
+  }
+
+  public prosek(): number {
+    if (this.ocene.length === 0) {
+      return 0.0;
+    }
+
+    return this.ocene.reduce((prev, next) => prev + next) / this.ocene.length;
+  }
+
+  public onDodajOcenu(): void {
+    if (Number.isNaN(Number.parseInt(this.ocenaStr))) {
+      this.ocenaStr = "";
+
+      return;
+    }
+
+    const ocenaNum: number = Number.parseInt(this.ocenaStr);
+
+    if (ocenaNum < 5 || ocenaNum > 10) {
+      this.ocenaStr = "";
+
+      return;
+    }
+
+    this.ocene.push(ocenaNum);
+
+    this.ocenaStr = "";
+  }
+}
