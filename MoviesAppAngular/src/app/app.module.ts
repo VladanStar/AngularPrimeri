be723710d1b6f0b8e37af64334d73f0d59db2ea5
VladanStar <vladancupric@gmail.com>
2023-02-21 13:28:47 +0100

    Add

--- MoviesAppAngular/src/app/app.module.ts
+++ MoviesAppAngular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
 import { AddMoviesComponent } from './components/add-movies/add-movies.component';
 import { MoviesHomeComponent } from './components/movies-home/movies-home.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolBarComponent,
     AddMoviesComponent,
-    MoviesHomeComponent
+    MoviesHomeComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ MoviesAppAngular/src/app/components/button/button.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+
+@Input()  text:string=" ";
+@Output()btnClick= new EventEmitter();
+
+constructor(){
+
+}
+
+onClick():void{
+this.btnClick.emit()
+}
+
+}
