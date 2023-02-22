e54cf597e820fbb2d986778b0368c5d54eaeca4d
VladanStar <vladancupric@gmail.com>
2023-02-22 11:04:34 +0100

    Add

--- PrenosPodataka/src/app/app.component.ts
+++ PrenosPodataka/src/app/app.component.ts
@@ -10,6 +10,20 @@ export class AppComponent {
   // {type:"server",name:"TestServer",content:"Just a Test"}
 ];
 
+onServerAdded(serverData:{serverName:string, serverContent:string}) {
+  this.serverElements.push({
+    type:'server',
+    name: serverData.serverName,
+    content:serverData.serverContent,
+ });
+}
 
+onBlueprintAdded(blueprintData:{serverName:string, serverContent:string}) {
+  this.serverElements.push({
+    type: 'blueprint',
+    name:blueprintData.serverName,
+    content: blueprintData.serverContent,
+  });
+}
 
 }
