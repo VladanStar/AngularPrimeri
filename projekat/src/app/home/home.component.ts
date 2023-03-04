06a973dc2ebe06d4a09c40fcc57571da2ad38773
VladanStar <vladancupric@gmail.com>
2023-03-04 17:08:01 +0100

    Add

--- projekat/src/app/home/home.component.ts
+++ projekat/src/app/home/home.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { count, interval } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {  interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+
+  private firstObsSubscription: Subscription = new Subscription;
+
 constructor(){};
 ngOnInit(): void {
-interval(1000).subscribe(count=>{
+this.firstObsSubscription= interval(1000).subscribe(count=>{
 console.log(count)
 })
+}
+ngOnDestroy(): void {
+this.firstObsSubscription.unsubscribe()
 }
 
 }
