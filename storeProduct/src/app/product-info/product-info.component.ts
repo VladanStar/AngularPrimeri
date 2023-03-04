213acfa3af2747e42cd9bb75a4751c8435b13827
VladanStar <vladancupric@gmail.com>
2023-03-04 20:20:20 +0100

    Add

--- storeProduct/src/app/product-info/product-info.component.ts
+++ storeProduct/src/app/product-info/product-info.component.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
-import { Product } from '../models/product.model';
+// import { Product } from '../models/product.model';
+import { ActivatedRoute } from '@angular/router';
+// import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-info',
   templateUrl: './product-info.component.html',
-  styleUrls: ['./product-info.component.css']
+  styleUrls: ['./product-info.component.css'],
 })
-export class ProductInfoComponent {
-  public products:Product[];
+export class ProductInfoComponent implements OnInit {
+   public product:any;
+  public paramMapSub: any;
 
-  constructor( private productService:ProductService){
-  this.products=this.productService.getProducts();
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {
 
   }
+
   ngOnInit(): void {
 
+    this.paramMapSub = this.route.paramMap.subscribe((params) => {
+console.log(this.paramMapSub)
+      const pId: number = Number(params.get('productId'));
+
+      this.productService.getProducts().forEach((p) => {
+console.log(this.product)
+        if (p.productId === pId) {
+          this.product = p;
+        }
+      });
+    });
   }
 }
