6b2816d4992959b7fb0510267eb387ddefb1ff18
VladanStar <vladancupric@gmail.com>
2023-03-04 17:20:47 +0100

    Add

---
+++ storeProduct/src/app/product-list/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+constructor(){}
+ngOnInit(): void {
+
+}
+
+}
