8ccdd3ec8453103b8bca8fb21ff68e93eec2eaf8
VladanStar <vladancupric@gmail.com>
2023-03-06 21:20:00 +0100

    Add

--- reactivne-forme/src/app/app.component.ts
+++ reactivne-forme/src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'reactivne-forme';
 singupForm!:FormGroup;
 
-genders=['male', 'female']
+genders=['male', 'female'];
+
+ngOnInit(): void {
+this.singupForm=new FormGroup({
+'username':new FormControl(null, Validators.required),
+'email':new FormControl(null, [Validators.required, Validators.email]),
+'gender':new FormControl('male'),
+
+});
 }
+onSubmit(){
+console.log(this.singupForm)
+}
+}
+
+
