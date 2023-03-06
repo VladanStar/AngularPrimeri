9d86773db9be4618bf0285ee371617117cedd3c2
VladanStar <vladancupric@gmail.com>
2023-03-06 19:24:37 +0100

    Add

--- reactivne-forme/src/app/app.component.ts
+++ reactivne-forme/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactivne-forme';
+singupForm!:FormGroup;
 
 genders=['male', 'female']
 }
--- reactivne-forme/src/app/app.module.ts
+++ reactivne-forme/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{ReactiveFormsModule}from'@angular/forms';
 import { AppComponent } from './app.component';
+// import{HttPModule}from'@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
