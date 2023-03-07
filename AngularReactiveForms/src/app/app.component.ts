bd2b550180aecc3af4b3bbb43ed0784dd36b5786
VladanStar <vladancupric@gmail.com>
2023-03-07 20:54:27 +0100

    Add

--- AngularReactiveForms/src/app/app.component.ts
+++ AngularReactiveForms/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularReactiveForms';
+export class AppComponent implements OnInit {
+
+
+ registrationForm!:FormGroup;
+
+ngOnInit(): void {
+this.registrationForm=new FormGroup( {
+  'username':new FormControl(null, Validators.required),
+'email':new FormControl(null, [Validators.required, Validators.email]),
+'conpassword':new FormControl(null, Validators.required),
+   })
+}
+
 }
