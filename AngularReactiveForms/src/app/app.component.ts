a20f26552ca549e5228c57cf813e96290aed3b0f
VladanStar <vladancupric@gmail.com>
2023-03-08 09:59:10 +0100

    Add

--- AngularReactiveForms/src/app/app.component.ts
+++ AngularReactiveForms/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 // import { forbiddenNameValidators } from './shared/user-name.validator';
 import { ForbiddenNameValidator } from './shared/user-name.validator';
-
+import { PasswordValidator } from './shared/password.validator';
 
 @Component({
   selector: 'app-root',
@@ -11,46 +11,58 @@ import { ForbiddenNameValidator } from './shared/user-name.validator';
 })
 export class AppComponent implements OnInit {
   //  registrationForm!: FormGroup;
-constructor(private fb:FormBuilder){}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
-    registrationForm=this.fb.group({
-username:['', [Validators.required, Validators.minLength(5), ForbiddenNameValidator(/admin/)]],
-password:['', [Validators.required, Validators.minLength(5)]],
-conpassword:['', Validators.required],
-addressForm:this.fb.group({
-city:['', Validators.required],
-state:['', Validators.required],
-postcode:['', Validators.required]
-})
-})
-    // this.registrationForm = new FormGroup({
-    //   username: new FormControl(null, Validators.required),
-    //   password: new FormControl(null, [Validators.required]),
-    //   conpassword: new FormControl(null, Validators.required),
-    //   addressForm: new FormGroup({
-    //     city: new FormControl(null, Validators.required),
-    //     state: new FormControl(null, Validators.required),
-    //     postcode: new FormControl(null, Validators.required),
-    //   }),
-    // });
+  registrationForm = this.fb.group(
+    {
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          ForbiddenNameValidator(/admin/),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      conpassword: ['', Validators.required],
+      addressForm: this.fb.group({
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        postcode: ['', Validators.required],
+      }),
+    },
+    { validator: PasswordValidator }
+  );
+
+  // this.registrationForm = new FormGroup({
+  //   username: new FormControl(null, Validators.required),
+  //   password: new FormControl(null, [Validators.required]),
+  //   conpassword: new FormControl(null, Validators.required),
+  //   addressForm: new FormGroup({
+  //     city: new FormControl(null, Validators.required),
+  //     state: new FormControl(null, Validators.required),
+  //     postcode: new FormControl(null, Validators.required),
+  //   }),
+  // });
 
   onLoadApi() {
-this.registrationForm.setValue({//patchValue samo jedan deo obrasca
-username:"Vladan",
-password:"Filter74",
-conpassword:'Fulter74',
-addressForm:{
-city:"Kragujevac",
-state:"Serbia",
-postcode:'34000'
-}
-})
+    this.registrationForm.setValue({
+      //patchValue samo jedan deo obrasca
+      username: 'Vladan',
+      password: 'Filter74',
+      conpassword: 'Fulter74',
+      addressForm: {
+        city: 'Kragujevac',
+        state: 'Serbia',
+        postcode: '34000',
+      },
+    });
   }
-onSubmit(){
-console.log(this.registrationForm)
-}
-get userName() {
-  return this.registrationForm.get('username');
-}
+  onSubmit() {
+    console.log(this.registrationForm);
+  }
+  get userName() {
+    return this.registrationForm.get('username');
+  }
 }
