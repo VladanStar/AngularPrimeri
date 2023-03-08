f51b5ede5591829da93047f575e226f364d27d3c
VladanStar <vladancupric@gmail.com>
2023-03-08 07:43:05 +0100

    Add

--- reactive-forms/src/app/custom-validators.ts
+++ reactive-forms/src/app/custom-validators.ts
@@ -1,10 +1,10 @@
 import { FormControl } from '@angular/forms';
 
 export class CustomValidators {
-  static invalidProjectName(control: FormControl): { [s:string]:boolean} {
+  static invalidProjectName(control: FormControl): { [s:string]:boolean | null} {
     if (control.value === 'Test') {
       return { 'invalidProjectName': true };
     }
-    return null!;
+    return null as any;
   }
 }
