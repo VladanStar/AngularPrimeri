541f5fe80fd7aff1b45b48c4f7218a3e51229687
VladanStar <vladancupric@gmail.com>
2023-03-11 17:05:33 +0100

    Add

--- firebase9app/src/index.js
+++ firebase9app/src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello from index.js');
+// console.log('Hello from index.js');
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjrTr7Nuu417KFsruYjV10oZuSupTiRfE",
+  authDomain: "fir-app-ec4a2.firebaseapp.com",
+  projectId: "fir-app-ec4a2",
+  storageBucket: "fir-app-ec4a2.appspot.com",
+  messagingSenderId: "999254885727",
+  appId: "1:999254885727:web:0fd32ff62aa80eb30f62ea",
+  measurementId: "G-HL35MJR4N0"
+};
