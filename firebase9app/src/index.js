ae72111342398c9b1b7c03193707eb88784d8ee6
VladanStar <vladancupric@gmail.com>
2023-03-11 18:53:37 +0100

    Add

--- firebase9app/src/index.js
+++ firebase9app/src/index.js
@@ -1,9 +1,9 @@
 
-import {initializeApp }from "firebase/app";
-
+import { initializeApp } from 'firebase/app'
 import {
-  getFirestore, collection, getDocs
-} from 'firebase/firestore'
+  getFirestore, collection, getDocs,
+  addDoc, deleteDoc, doc
+} from 'firebase/firestore';
 // console.log('Hello from index.js');
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -41,4 +41,32 @@ getDocs(colRef)
   })
   .catch(err => {
     console.log(err.message)
+  })
+  
+
+  // adding docs
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+})
+
+// deleting docs
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+  deleteDoc(docRef)
+    .then(() => {
+      deleteBookForm.reset()
+    })
   })
