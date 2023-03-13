5be55aa5aadf514d57f8b7fd47e8c8c1ba1fe444
VladanStar <vladancupric@gmail.com>
2023-03-13 16:55:44 +0100

    Add

--- firebase-project/src/app/app.module.ts
+++ firebase-project/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { CustomerTableComponent } from './customer-table/customer-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateCustomerComponent
+    CreateCustomerComponent,
+    CustomerTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ firebase-project/src/app/customer-table/customer-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-table',
+  templateUrl: './customer-table.component.html',
+  styleUrls: ['./customer-table.component.css']
+})
+export class CustomerTableComponent {
+
+}
