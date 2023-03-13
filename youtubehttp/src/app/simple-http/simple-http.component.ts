91df89f952c9ee391648f199037f090239b1be06
VladanStar <vladancupric@gmail.com>
2023-03-13 09:41:59 +0100

    Add

--- youtubehttp/src/app/simple-http/simple-http.component.ts
+++ youtubehttp/src/app/simple-http/simple-http.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import{HttpClient}from "@angular/common/http";
 @Component({
   selector: 'app-simple-http',
   templateUrl: './simple-http.component.html',
   styleUrls: ['./simple-http.component.css']
 })
-export class SimpleHttpComponent {
+export class SimpleHttpComponent implements OnInit {
 loading:boolean =true;
-data:[]=[]
+data:Object ={};
+constructor(private http: HttpClient){};
+ngOnInit(): void {
 
-  makeRequest(){
-
-  }
+}
+makeRequest(): void {
+  this.loading = true;
+  this.http
+    .get('https://jsonplaceholder.typicode.com/posts/1')
+    .subscribe(data => {
+      this.data = data;
+      this.loading = false;
+    });
+}
 }
